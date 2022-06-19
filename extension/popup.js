f93c565a2c8916823bda6e4b35ab56e1b15f6069
haikudoggies <haikudoggies@gmail.com>
2022-06-19 09:19:13 -0700

    initial commit

--- extension/popup.js
+++ extension/popup.js
@@ -1,5 +1,7 @@
-let notepad = document.getElementById("note1");
+var note_id =  "note1";
+var notepad = document.getElementById(note_id);
 notepad.focus();
+
 
 // grab saved data in notepad
 chrome.storage.local.get(['notepad'], function (data) {
@@ -12,3 +14,19 @@ notepad.addEventListener('keyup', function (event) {
         console.log("Notepad updated with", event.target.innerHTML);
     });
 });
+
+document.addEventListener('click',function(e){
+    if(e.target && e.target.id === 'bro'){
+        //do something
+        notepad.style.backgroundColor = 'yellow';
+    }
+    else if(e.target && e.target.id === 'add'){
+        notepad.style.backgroundColor = 'blue';
+        var button = document.createElement('button');
+        button.type = 'button';
+        button.innerHTML = 'added button';
+        var column = document.getElementById('notelist');
+        column.insertBefore(button,e.target);
+    }
+});
+
