4f1d02bdb247ed18fd9bb0ada89b38625ee8959a
haikudoggies <haikudoggies@gmail.com>
2022-06-20 16:07:14 -0700

    able to add new buttons with delete button

--- extension/popup.js
+++ extension/popup.js
@@ -1,7 +1,7 @@
+// make sure to keep saving constanty and getting
+
 // list of the notes that are saved
 let notelist = [];
-// list of the buttons that correspond to each note
-let notebuttonlist = [];
 // how many buttons were created
 let createdbuttons = 0;
 
@@ -12,38 +12,62 @@ let leftcolumn = document.getElementById('notelist');
 let rightcolumn = document.getElementById('notebox');
 let addbutton = document.getElementById('addbutton');
 
-// grab saved data
-/*chrome.storage.local.get(['notelist_saved','notebuttonlist_saved'], function (saveddata) {
-    // get the saved buttons
-    notebuttonlist = JSON.parse(saveddata.notebuttonlist_saved);
-    for (let i = 0; i < notebuttonlist.length; i++){
-        let button = document.createElement('button');
-        button.type = 'button';
-        button.className = 'button';
-        button.id = notebuttonlist[i];
-        button.innerHTML = button.id;
-        leftcolumn.insertBefore(button,addbutton);
+// initial get saved data
+chrome.storage.local.get(['notelist_saved','createdbuttons_saved'], function (saveddata) {
+    if (saveddata.notelist_saved){
+        notelist = JSON.parse(saveddata.notelist_saved);
+        for (let i = 0; i < notelist.length; i++){
+            // create button, along with a delete that corresponds to it inside a new corresponding div
+            let newdiv = document.createElement('div');
+            newdiv.type = 'div';
+            newdiv.id = notelist[i].name;
+            newdiv.className = 'totalbutton';
+            leftcolumn.insertBefore(newdiv,addbutton);
+
+            // new button
+            let button = document.createElement('button');
+            button.type = 'button';
+            button.className = 'button';
+            button.id = notelist[i].name + 'b';
+            button.innerHTML = button.id + "ruh";
+            newdiv.appendChild(button);
+
+            // new delete
+            let deletebut = document.createElement('button');
+            deletebut.type = 'button';
+            deletebut.className = 'deletebut';
+            deletebut.id = notelist[i].name + 'd';
+            deletebut.innerHTML = '-';
+            newdiv.appendChild(deletebut);
+        }
+        if (notelist.length !== 0) {
+        // initial notepad is the first one
+            let note = document.createElement('div');
+            note.className = 'note';
+            note.id = notelist[0].name;
+            note.innerHTML = notelist[0].notetext;
+            note.contentEditable = 'true';
+            rightcolumn.appendChild(note);
+            notepad = note;
+        }
     }
-    // set initial notepad, button 1
+    if (saveddata.createdbuttons_saved){
+        createdbuttons = saveddata.createdbuttons_saved;
+    }
+})
 
 
-
-    // saved notes?
-    notelist = JSON.parse(saveddata.notelist_saved);
-  //  notepad.innerHTML = data.notepad;
-   // notepad. =
-
-});*/
-
-if (notepad){
-    // event listener for whenever key is typed
-    notepad.addEventListener('keyup', function (event) {
-        chrome.storage.local.set({notepad: event.target.innerHTML}, () => {
-            // save to specific notepad
-            console.log("Notepad updated with", event.target.innerHTML);
-        });
-    });
-}
+// if there exists a notepad
+// event listener for whenever key is typed
+document.addEventListener('keyup', function (e) {
+        if (e.target && e.target.id === notepad.id){
+            let currenthtml = e.target.innerHTML;
+            // match the id of the current notepad with the one inside notelist
+            let noteindex = notelist.findIndex(x => x.name === notepad.id);
+            notelist[noteindex].notetext = currenthtml;
+            chrome.storage.local.set({ notelist_saved: JSON.stringify(notelist), createdbuttons_saved: createdbuttons}, () => {});
+        }
+});
 
 document.addEventListener('click',function(e){
     if(e.target && e.target.id === 'addnote'){
@@ -55,7 +79,7 @@ document.addEventListener('click',function(e){
         leftcolumn.insertBefore(newdiv,addbutton);
 
         // add the id of the new button
-        notebuttonlist.push(newdiv.id);
+       // notebuttonlist.push(newdiv.id);
 
         // new button
         let button = document.createElement('button');
@@ -78,6 +102,7 @@ document.addEventListener('click',function(e){
         note.className = 'note';
         note.id = createdbuttons;
         note.innerHTML = note.id;
+        note.contentEditable = 'true';
         // if notepad exists, replace it
         if (notepad) {
             rightcolumn.replaceChild(note, notepad);
@@ -93,28 +118,29 @@ document.addEventListener('click',function(e){
             notetext: note.id
         }
         notelist.push(note_object);
-
-        // save new note
-        chrome.storage.local.set({notelist_saved: JSON.stringify(notelist), notebuttonlist_saved: JSON.stringify(notebuttonlist)}, () => {
-            // save to specific notepad
-            console.log("saved");
-        });
-
         createdbuttons++;
+
+        // save data
+        chrome.storage.local.set({notelist_saved: JSON.stringify(notelist),createdbuttons_saved: createdbuttons}, () => {});
 
     }
     // if click on note button, then show the corresponding note
     else if(e.target && e.target.className === 'button'){
-        // get the notelist
-        chrome.storage.local.get(['notelist_saved','notebuttonlist_saved'], function (saveddata) {
-            notelist = JSON.parse(saveddata.notelist_saved);
+        // get the notelist because of updates while popup is open
+        chrome.storage.local.get(['notelist_saved','createdbuttons_saved'], function (saveddata) {
+            if (saveddata.notelist_saved) {
+                notelist = JSON.parse(saveddata.notelist_saved);
+            }
         });
         // find the note that corresponds to the button id
-        let notehtml = notelist[notelist.findIndex(x => x.name === e.target.id)].notetext;
+        let noteget = notelist[notelist.findIndex(x => (x.name + 'b') === e.target.id)];
+        let notehtml = noteget.notetext;
+        let notename = noteget.name;
         let note = document.createElement('div');
         note.className = 'note';
-        note.id = e.target.id;
+        note.id = notename;
         note.innerHTML = notehtml;
+        note.contentEditable = 'true';
         // replace the current note with the new note
         // if notepad exists, replace it
         if (notepad) {
@@ -125,6 +151,18 @@ document.addEventListener('click',function(e){
             rightcolumn.appendChild(note);
         }
         notepad = note;
+    }
+    else if(e.target && e.target.className === 'deletebut'){
+        // find the div that contains this and delete it
+        e.target.parentNode.remove();
+        // find the index of where the corresponding note is in the array
+        let index = notelist.findIndex(x => (x.name + 'd') ===  e.target.id);
+        notelist.splice(index,1);
+        chrome.storage.local.set({notelist_saved: JSON.stringify(notelist),createdbuttons_saved: createdbuttons}, () => {});
+        if (notepad) {
+            rightcolumn.removeChild(notepad);
+            notepad = undefined;
+        }
     }
 });
 
